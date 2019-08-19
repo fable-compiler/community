967906133bf1fb2d9b94b4ec07d6463f0e43a6ac
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2019-08-19 14:27:06 +0200

    Initial commit

---
+++ src/App.fs
@@ -0,0 +1,135 @@
+module App
+
+(**
+ The famous Increment/Decrement ported from Elm.
+ You can find more info about Elmish architecture and samples at https://elmish.github.io/
+*)
+
+open System.Text.RegularExpressions
+open Elmish
+open Elmish.React
+open Fable.React
+open Fable.React.Props
+open Fable.SimpleHttp
+open Fulma
+open Global
+
+let init(): Model * Cmd<Msg> =
+    let model = Model.Empty
+    let cmd =
+        async {
+            let! (_, json) = Http.get "community.json"
+            return ResourcesLoaded json
+        } |> Cmd.OfAsync.result
+    model, cmd
+
+let shuffle (org:_ list) =
+    let rng = System.Random()
+    let arr = Array.ofList org
+    let max = arr.Length - 1
+    let randomSwap (arr:_[]) i =
+        let pos = rng.Next(max)
+        let tmp = arr.[pos]
+        arr.[pos] <- arr.[i]
+        arr.[i] <- tmp
+        arr
+    [|0..max|] |> Array.fold randomSwap arr |> Array.toList
+
+// TODO: Truncate?
+let resetVisibleResources (model:Model) =
+    { model with VisibleResources = model.Resources }
+
+let update (msg:Msg) (model:Model) =
+    match msg with    
+    | ToggleBurger ->
+        { model with BurgerOpen = not model.BurgerOpen }, Cmd.none
+    | ResourcesLoaded json ->
+        let resources = Resource.FromJsonArray json |> shuffle
+        { model with Resources = resources }
+        |> resetVisibleResources, Cmd.none
+    | SearchTextUdpdated text ->
+        let terms: string list =
+            match text.Trim() with
+            | text when text.Length > 2 ->
+                // TODO: Discard terms if their length is less than 2?
+                Regex.Split(text, @"(?:\s*,\s*)|\s+") |> Array.toList
+            | _ -> []
+        let model =
+            match terms with
+            | [] -> resetVisibleResources model
+            | terms ->
+                let regex =
+                    Regex(terms
+                          |> Seq.map (fun x -> "(?=.*" + Regex.Escape(x) + ")")
+                          |> String.concat "", RegexOptions.IgnoreCase)
+                { model with VisibleResources =
+                                model.Resources |> List.filter (fun r ->
+                                    regex.IsMatch(r.title + "\n" + r.description)) }
+        { model with SearchText = text }, Cmd.none
+
+// VIEW (rendered with React)
+
+let footer() =
+    footer [
+      Class "footer"
+      Style [BackgroundColor "dodgerblue"]
+    ] [ 
+      Content.content [] [
+        p [] [
+          str "Fable "
+          a [ Href "https://github.com/fable-compiler/Fable" ]
+            [ str "source code" ]
+          str " is licensed "
+          a [ Href "http://opensource.org/licenses/mit-license.php" ]
+            [ str "MIT" ]
+          str "."
+    ] ] ]
+
+let searchBox (model:Model) dispatch =
+    Field.div [ ]
+        [ //Label.label [ ] [ str "Search" ]
+          Control.div [ ]
+            [ Input.text [ Input.Placeholder "Search resources"
+                           Input.Props [AutoFocus true]
+                           Input.Value model.SearchText
+                           Input.OnChange (fun ev -> SearchTextUdpdated ev.Value |> dispatch)
+                         ]
+            ]
+        ]
+
+let resourceItem (r: Resource) =
+    div [ Class "resource-item"
+          Key (string r.hash) ]
+        [ Heading.h5 [] [
+            a [Href r.link] [str r.title]
+            str "  "
+            Tag.tag [ Tag.Color IsSuccess ] [str <| r.category.ToString().ToLower()]
+          ]
+          Heading.h6 [Heading.IsSubtitle] [str r.description] ]
+
+let view (model:Model) dispatch =
+  fragment []
+      [ Navbar.view model.BurgerOpen dispatch
+        Section.section [Section.CustomClass "main-section"] [
+            Columns.columns [] [
+                Column.column [
+                    Column.Width (Screen.All, Column.IsThreeFifths)
+                    Column.Offset (Screen.All, Column.IsOneFifth)
+                ] [
+                    Heading.h4 [Heading.CustomClass "intro-text"] [
+                        str "Find resources from the community!"                        
+                    ]
+                    searchBox model dispatch
+                    ul [Class "resource-list"]
+                        (List.map resourceItem model.VisibleResources)
+                ]
+            ]
+        ]
+        footer()
+      ]
+
+// App
+Program.mkProgram init update view
+|> Program.withReactSynchronous "elmish-app"
+// |> Program.withConsoleTrace
+|> Program.run
---
+++ src/Global.fs
@@ -0,0 +1,44 @@
+module Global
+
+open Fable.Core
+
+[<StringEnum>]
+type ResourceCategory =
+    | Library
+    | Tool
+    | Tutorial
+    | Video
+
+let private stringHash (s: string) =
+    let mutable h = 5381
+    for i = 0 to s.Length - 1 do
+        h <- (h * 33) ^^^ (int s.[i])
+    h
+
+type Resource =
+    { title: string
+      description: string
+      link: string
+      category: ResourceCategory
+      hash: int }
+      static member FromJsonArray(json: string) =
+        let rs: Resource[] = JS.JSON.parse(json) :?> _
+        rs |> Seq.map (fun r ->
+            { r with hash = stringHash(r.title + r.link) })
+        |> List.ofSeq
+
+type Model =
+    { BurgerOpen: bool
+      Resources: Resource list
+      VisibleResources: Resource list
+      SearchText: string }
+    static member Empty =
+        { BurgerOpen = false
+          Resources = []
+          VisibleResources = []
+          SearchText = "" }
+
+type Msg =
+    | ToggleBurger
+    | ResourcesLoaded of string
+    | SearchTextUdpdated of string
